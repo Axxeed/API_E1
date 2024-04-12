3e8c37a31acdf804083571cae79a1ed33f5d980f
Axxeed <105978339+Axxeed@users.noreply.github.com>
2024-04-12 15:39:49 +0200

    Add files via upload

--- api.py
+++ api.py
@@ -1,23 +1,33 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import pickle
-
-class InputData(BaseModel):
-
-    code_post:float
-    surface: float
-    nb_piece: float
-    code_reg: float
-
-
-with open('xgb.pkl', 'rb') as file:
-    model = pickle.load(file)
+import joblib
+import pandas as pd
+import uvicorn
 
 app = FastAPI()
 
-@app.post("/predict")
-def predict(data: InputData):
 
-    input_data = [data.code_post, data.surface, data.nb_piece, data.code_reg]
-    prediction = model.predict([input_data])
-    return {"prediction": prediction[0]}
+model = joblib.load("xgb.pkl")
+
+@app.post("/predict")
+def predict(
+    Code_postal_x: float,
+    Surface_Carrez_du_1er_lot: float,
+    Nombre_pieces_principales: float,
+    code_region: float,
+):
+    modeldata_dict = {
+        "Code postal_x": Code_postal_x,
+        "Surface Carrez du 1er lot": Surface_Carrez_du_1er_lot,
+        "Nombre pieces principales": Nombre_pieces_principales,
+        "code_region": code_region,
+    }
+
+    index = [0]
+    model_data_df = pd.DataFrame(modeldata_dict, index=index)
+
+    result = model.predict(model_data_df).tolist()
+
+    return {"result": result}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, debug=True)
